f5ebfce2dd9901efc41cb513acb0e71b1e1422d3
ChrissSav <dai17023@uom.edu.gr>
2020-09-09 18:17:55 +0300

    first commit

---
+++ src/main/java/com/example/BaseApi/BaseApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.BaseApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BaseApi/BaseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BaseApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
